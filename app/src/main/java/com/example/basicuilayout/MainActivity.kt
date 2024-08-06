4a932a74281a425b75214644c1209b379e77c288
Raushansingh5 <rajatraushan5@gmail.com>
2024-08-06 13:53:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/basicuilayout/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.basicuilayout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button:Button=findViewById(R.id.button)
+        button.setOnClickListener{
+            val intent=Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
